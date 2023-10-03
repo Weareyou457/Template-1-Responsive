6b73e74805cde1dd04824ffa12667ed977aff254
Weareyou457 <alfaizmalwa567@gmail.com>
2023-10-03 14:14:34 +0530

    first commit

---
+++ src/COMPONENT/EducationSports.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Popularpost from '../Sidebar/Popularpost'
+import Article from '../Template/Article'
+import '../responsive.css';
+
+function EducationSports({page_name,agencyDetails,breakingNews}) {
+    return (
+        <div className='row' style={{padding:"12px"}}>
+            <div className='col-md-8 col-sm-8 p'>
+                <Article page_name={'Home_Page'} agencyDetails={agencyDetails} breakingNews={breakingNews}/>
+            </div>
+            <div className='col-md-4 col-sm-4'>
+                <Popularpost page_name={'Home_Page'} agencyDetails={agencyDetails} breakingNews={breakingNews}/>
+            </div>
+        </div>
+    )
+}
+
+export default EducationSports
